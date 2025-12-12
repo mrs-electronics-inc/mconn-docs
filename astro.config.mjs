118ce5590b4d735c5de728d6318894bb59d45a80
Bennett Moore <bennett.moore@mrs-electronics.com>
2025-12-12 12:16:08 -0500

    chore: run formatter again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,56 +1,64 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://mconn.mrs-electronics.dev",
-	integrations: [
-		starlight({
-			plugins: [],
-			title: 'MConn Documentation',
-			social: [
-				{ icon: 'github', label: 'GitHub', href: 'https://github.com/mrs-electronics-inc/mconn-docs' },
-			],
-			sidebar: [
-				{
-					label: "User Information and Safety",
-					autogenerate: { directory: "user-information-and-safety" },
-				},
-				{
-					label: "Getting Started",
-					autogenerate: { directory: "getting-started" },
-				},
-				{
-					label: "Understanding MConn Hardware",
-					autogenerate: { directory: "understanding-mconn-hardware" },
-				},
-				{
-					label: "MConn Programming",
-					items: [
-						{
-							label: "Qt QML",
-          					link: "/mconn-programming/qt-qml"
-						},
-						{
-							label: "MConn Modules Guide",
-							autogenerate: { directory: "mconn-programming/mconn-modules-guide" }
-						},
-						{
-							label: "Exploring Reference App",
-							autogenerate: { directory: "mconn-programming/exploring-reference-app" }
-						}
-					]
-				},
-				{
-					label: "Starting User Application",
-					autogenerate: { directory: "starting-user-application" },
-				},
-				{
-					label: "Other",
-					autogenerate: { directory: "other" },
-				},
-			],
-		}),
-	],
+  site: "https://mconn.mrs-electronics.dev",
+  integrations: [
+    starlight({
+      plugins: [],
+      title: "MConn Documentation",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/mrs-electronics-inc/mconn-docs",
+        },
+      ],
+      sidebar: [
+        {
+          label: "User Information and Safety",
+          autogenerate: { directory: "user-information-and-safety" },
+        },
+        {
+          label: "Getting Started",
+          autogenerate: { directory: "getting-started" },
+        },
+        {
+          label: "Understanding MConn Hardware",
+          autogenerate: { directory: "understanding-mconn-hardware" },
+        },
+        {
+          label: "MConn Programming",
+          items: [
+            {
+              label: "Qt QML",
+              link: "/mconn-programming/qt-qml",
+            },
+            {
+              label: "MConn Modules Guide",
+              autogenerate: {
+                directory: "mconn-programming/mconn-modules-guide",
+              },
+            },
+            {
+              label: "Exploring Reference App",
+              autogenerate: {
+                directory: "mconn-programming/exploring-reference-app",
+              },
+            },
+          ],
+        },
+        {
+          label: "Starting User Application",
+          autogenerate: { directory: "starting-user-application" },
+        },
+        {
+          label: "Other",
+          autogenerate: { directory: "other" },
+        },
+      ],
+    }),
+  ],
 });
